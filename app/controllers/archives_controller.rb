4bb7c153c4611acb0a0b4bbf3f8cdd84f0775227
Sebastian Olivares <olivares.heredia@gmail.com>
2024-12-03 09:32:05 +0100

    adding pagination logic done

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -2,22 +2,28 @@ require 'open-uri'
 require 'json'
 
 class ArchivesController < ApplicationController
-  before_action :fetch_year_data
   def index
     per_page = 30
-    page = (params[:page] || 1).to_i
-    @paginated_data = @apod_data[((page - 1) * per_page)...(page * per_page)]
-    @total_pages = (@apod_data.size / per_page.to_f).ceil
+    page = params[:page].present? ? params[:page].to_i : 0
+
+    if page == 0
+      end_date = Date.today
+      start_date = end_date - (per_page - 1)
+    else
+      end_date = Date.today - (page * per_page)
+      start_date = end_date - (per_page - 1)
+    end
+
+    @apod_data = fetch_range_data(start_date.to_s, end_date.to_s)
   end
 
   private
 
-  def fetch_year_data
+  def fetch_range_data(start_date, end_date)
     api_key = Rails.application.credentials.dig(:APOD, :api_key)
-    start_date = Date.new(2024, 11, 4)
-    end_date = Date.today
 
-    url = "https://api.nasa.gov/planetary/apod?api_key=#{api_key}&end_date=#{end_date}&start_date=#{start_date}"
+    url = "https://api.nasa.gov/planetary/apod?api_key=#{api_key}&start_date=#{start_date}&end_date=#{end_date}"
+    Rails.logger.info "Fetching URL: #{url}"
     response = URI.open(url).read
     @apod_data = JSON.parse(response).reverse
   end
