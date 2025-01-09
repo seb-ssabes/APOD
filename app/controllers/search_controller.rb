69e3a8f77c40c49f272b04bae0e7bb21d44c26a7
Sebastian Olivares <olivares.heredia@gmail.com>
2025-01-09 15:48:46 +0100

    img and iframe improved

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,10 +9,26 @@ class SearchController < ApplicationController
   def api_search
     query = params[:query].downcase
 
-    @apod_data = fetch_data
+    # @apod_data = fetch_data
 
-    filtered_results = @apod_data.select do |item|
-      item['title'].to_s.include?(query) || item['explanation'].to_s.include?(query) || item['copyright'].to_s.include?(query)
+    is_date = false
+    date_query = nil
+
+    begin
+      date_query = Date.parse(query).strftime('%Y-%m-%d')
+      is_date = true
+    rescue ArgumentError
+    end
+
+    if is_date
+      apod_data = fetch_data(date_query)
+      filtered_results = apod_data ? [apod_data] : []
+    else
+      filtered_results = @apod_data.select do |item|
+        item['title'].to_s.include?(query) ||
+        item['explanation'].to_s.include?(query) ||
+        item['copyright'].to_s.include?(query)
+      end
     end
 
     render json: {
@@ -26,9 +42,14 @@ class SearchController < ApplicationController
 
   private
 
-  def fetch_data
+  def fetch_data(date = nil)
     api_key = Rails.application.credentials.dig(:APOD, :api_key)
-    url = "https://api.nasa.gov/planetary/apod?api_key=#{api_key}&count=30"
+
+    if date
+      url = "https://api.nasa.gov/planetary/apod?api_key=#{api_key}&date=#{date}"
+    else
+      url = "https://api.nasa.gov/planetary/apod?api_key=#{api_key}&count=30"
+    end
 
     response = URI.open(url).read
     JSON.parse(response)
